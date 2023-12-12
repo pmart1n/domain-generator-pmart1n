7df4f72edc7098caba7327f3d8bb879d99cac34a
pmart1n <123256191+pmart1n@users.noreply.github.com>
2023-12-12 10:44:09 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#dominios").innerHTML = DomainGenerator();
 };
+
+let DomainGenerator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com", ".es", ".io"];
+  let resultado = [];
+
+  for (const i of pronoun) {
+    for (const k of adj) {
+      for (const o of noun) {
+        for (const p of dominio) {
+          resultado.push(`${i}${k}${o}${p}`);
+        }
+      }
+    }
+  }
+  return resultado.join(", ");
+};
+
+console.log(DomainGenerator());
